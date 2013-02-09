c2bb5f46b61db61a0b737b8a26f9aeac0b0a9168
özgür <hinoglu@gmail.com>
2013-02-09 18:06:13 +0200

    changed page structures

---
+++ main/messages.py
@@ -0,0 +1,10 @@
+# ~*~ encoding: utf-8 ~*~
+
+from django.utils.translation import ugettext_lazy as _
+
+action_messages = {
+    "page_created": _("Page created successfuly"),
+    "page_updated": _("Page updated successfuly"),
+    "page_reverted": _("Page reverted to revision %s"),
+}
+
